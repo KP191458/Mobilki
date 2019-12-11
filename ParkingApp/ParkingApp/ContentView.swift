fe7978e48b6d6c81832c7506b0ec9b8f0d529d2d
Konrad Pławik <kp@Konrads-iMac.local>
2019-12-11 22:22:20 +0100

    Initial commit.

---
+++ ParkingApp/ParkingApp/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  ParkingApp
+//
+//  Created by Konrad Pławik on 11/12/2019.
+//  Copyright © 2019 Konrad Pławik. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
